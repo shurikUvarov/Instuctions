d98c2893cecf9c75e8b85d58a352eead5bef6064
brick1ng5654 <Bricking5654Help@yandex.ru>
2025-03-05 04:30:42 +0300

    editor done
    editor done

---
+++ source/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'База знаний'
+copyright = '2025, uvatron.ru'
+author = 'uvatron.ru'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['myst_parser']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'ru'
+source_suffix = ['.rst', '.md']
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_show_sphinx = False
+# html_theme_options = {
+#     "footer_text": False  # Отключает стандартный текст в подвале
+# }
+html_show_sourcelink = False
